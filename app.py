a44a0d7f14496b41666fff6deb1fd766a1bf6e12
realxt2022 <pronoobpro25544@gmail.com>
2025-06-20 01:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,8 @@ def after_request(response):
     return response
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
